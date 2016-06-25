39d368056c029ad0211e4fbec903770de4aadaa1
lingjun <kognlingjun@gmail.com>
2016-06-25 16:25:58 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,26 @@ $(document).ready(function () {
         $("#nav_dropdown").toggle();
     });
 
-
+    
    
 		
+});
+
+jQuery(document).ready(function($) {
+    $('.my-slider').unslider({
+        animation: 'fade', 
+        autoplay: true, 
+        arrows: true,
+        nav:false,
+        arrows: {
+	//  Unslider default behaviour
+	prev: '<a class="unslider-arrow prev"><i class="material-icons">keyboard_arrow_left</i></a>',
+	next: '<a class="unslider-arrow next"><i class="material-icons">keyboard_arrow_right</i></a>',
+
+	//  Example: generate buttons to start/stop the slider autoplaying
+//	stop: '<a class="unslider-pause" />',
+//	start: '<a class="unslider-play">Play</a>'
+}
+
+    });
 });
